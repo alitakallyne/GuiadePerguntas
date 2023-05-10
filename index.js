d2befdb2393cb1d691271384dca691b546cfec2a
alitakallyne <alytakallyne@gmail.com>
2023-05-09 22:07:38 -0300

    Create connection database

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const connection = require("./database/database");
+const pergunta = require("./database/Pergunta");
+const Pergunta = require("./database/Pergunta");
+
+// Database configuration
+connection
+.authenticate()
+.then(() =>{
+    console.log("Connection feita como banco de dados");
+})
+.catch((msgErro) =>{
+    console.log(msgErro);
+})
 
 // Estou dizendo para o Express usar o EJS como View engine
 app.set('view engine','ejs');
@@ -11,7 +24,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get("/",(req, res) => {
-    res.render("index");
+    Pergunta.findAll({raw:true}).then(perguntas =>{
+        res.render("home",{
+            perguntas:perguntas       });
+    })
+    
 })
 
 app.get("/perguntar",(req, res) => {
@@ -22,8 +39,15 @@ app.post("/salvarpergunta",(req, res) => {
     var titulo = req.body.titulo;
     var descricao = req.body.descricao;
 
-    res.send("Formulario Recebido! titulo: " + titulo + " descricao: " + descricao);
-})
+    // res.send("Formulario Recebido! titulo: " + titulo + " descricao: " + descricao);
+    //Passando Para o banco de dados
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(()=> {
+        res.redirect("/");
+    });
+});
 
 app.listen(8080,()=>{console.log("App rodando!");});
 
